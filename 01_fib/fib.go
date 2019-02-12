5e4dd2b9116cd2f56a25a536161167659ae522ca
GLOBAL\telkarr1 <raghavendra.telkar@anz.com>
2019-02-12 09:31:38 +0530

    Fibonacci code fixes

--- 01_fib/fib.go
+++ 01_fib/fib.go
@@ -10,10 +10,10 @@ var out io.Writer = os.Stdout
 
 func main() {
 	fmt.Fprint(out, fib(7))
-	}
+}
 
-func fib(n int) int{
-	if n <=1 {
+func fib(n int) int {
+	if n <= 1 {
 		return n
 	}
 	return fib(n-1) + fib(n-2)
