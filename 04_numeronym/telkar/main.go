99f0534ec0374b74bf2122b1df480dbe20582448
RaghuTelkar <raghavendra.telkar@anz.com>
2019-02-13 14:18:06 +0530

    lab 4
    lab 4 code with tests

---
+++ 04_numeronym/telkar/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"os"
+)
+
+var out io.Writer = os.Stdout
+
+func numeronyms(vals ...string) []string {
+	numeronyms := []string{}
+	for _, val := range vals {
+		numeronyms = append(numeronyms, generateNumeronym(val))
+	}
+	return numeronyms
+}
+
+func generateNumeronym(input string) string {
+	inputLen := len(input)
+	//minimum 4 chars are required
+	if inputLen <= 3 {
+		return input
+	}
+
+	return fmt.Sprintf("%c%d%c", input[0], inputLen-2, input[inputLen-1])
+}
+
+func main() {
+	fmt.Fprint(out, numeronyms("accessibility", "Kubernetes", "abc"))
+}
